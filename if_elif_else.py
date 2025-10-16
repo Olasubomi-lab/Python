99745d18c387222f90690f0f59ea8ca261242da0
Olasubomi-lab <olasubomisalawu35@gmail.com>
2025-10-16 19:57:52 +0100

    Add files via upload

---
+++ if_elif_else.py
@@ -0,0 +1,14 @@
+marks = 39
+
+if marks >= 90:
+    print("You got an A")
+elif marks >= 80 and marks < 90:
+    print("You got a B")
+elif marks >= 70 and marks < 80:
+    print("You got a C")
+elif marks >= 60 and marks < 70:
+    print("You got a D")
+elif marks >= 50 and marks < 60:
+    print("You got a E")
+else:
+    print("You get a F")
---
+++ nested_if.py
@@ -0,0 +1,9 @@
+height = 179
+coins = 3
+
+if height >= 120:
+    print("You can ride the roller coaster")
+    if coins >= 10:
+        print("You have a fast pass u can skip the queue")
+else:
+    print("You cannot ride the roller coaster")
