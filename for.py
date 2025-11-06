1ddcedf551b7a98d84bc996ba68bc7b58e24fbd8
Olasubomi-lab <olasubomisalawu35@gmail.com>
2025-11-06 17:06:31 +0000

    Add files via upload

---
+++ for.py
@@ -0,0 +1,34 @@
+#Example 1
+for i in range(1,6):
+    print(i)
+
+# Example 2
+fruits = ["cherry","mango","pomogranette"]
+for i in fruits:
+    print(i)
+
+# Example 3
+for i in "hello":
+    print(i)
+print()
+
+# Example 4
+greeting = "hello"
+for i in greeting:
+    print(i)
+
+# Example 5
+for i in range(1,10,2):
+    print(i)
+
+# Example 6
+numbers = [1,2,3,4,5,6]
+for i in numbers:
+    if i%2 == 0:
+        print(i,"is even")
+        
+# Example 7
+colours = ["violet","indigo","blue"]
+
+for i in range(len(colours)):
+    print(i,colours[i])
