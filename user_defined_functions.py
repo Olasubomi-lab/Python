eff31107edef415bd9c1fd6e306448e9dae299b1
Olasubomi-lab <olasubomisalawu35@gmail.com>
2025-12-12 16:59:08 +0000

    Add files via upload

---
+++ user_defined_functions.py
@@ -0,0 +1,36 @@
+# Example 1
+# Creating a function
+def greetings():
+    print("hello my friend")
+
+# Calling the fuction
+greetings()
+
+
+
+# Example 2 
+def fruits():
+    print("mango")
+
+fruits()
+
+# Example 3 - function with arguments and parameteres
+
+def add(a,b):
+    print(a+b)
+
+add(36,23)
+
+
+# Example4 - function for multiplying three numbers
+
+def multiply(c,d,e):
+    print(c*d*e)
+multiply(2,8,5)
+
+
+# Example 5 - Joining two strings
+
+def s_add(b,g):
+    print(f"{b} {g}")
+s_add("rice","box")
