8fc2e5f74d2b52d017e8735a1a642dcbebbd3d93
Olasubomi-lab <olasubomisalawu35@gmail.com>
2025-12-26 17:11:47 +0000

    Add files via upload

---
+++ globalscope.py
@@ -0,0 +1,23 @@
+# Example 1
+age = 5
+def num():
+    print(age)
+num()
+print(age)
+
+
+# Example 2
+message = "hello"
+def greet():
+    print(message)
+greet()
+print(message)
+
+
+# Example 3
+num = 4
+def square():
+    squared = num*num
+    print(squared)
+square()
+print(num)
