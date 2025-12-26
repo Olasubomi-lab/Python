ab2cc3147b6a53134c7a41acb1e7f2cd40b2cd0e
Olasubomi-lab <olasubomisalawu35@gmail.com>
2025-12-26 16:56:52 +0000

    Add files via upload

---
+++ localscore.py
@@ -0,0 +1,22 @@
+# Example 1
+def num():
+    age = 5
+    print(age)
+num()
+
+# print(age) - this will give an error
+
+# Example 2
+def greet():
+    message = "hello"
+    print(message)
+greet()
+
+# print(message)
+
+# Example 3
+def square(num):
+    squared=num*num
+    print(squared)
+square(4)
+# print(squared)
