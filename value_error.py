77063a7c29a2f5a4897971aa4271d3f421318934
Olasubomi-lab <olasubomisalawu35@gmail.com>
2026-02-05 17:58:43 +0000

    Add files via upload

---
+++ value_error.py
@@ -0,0 +1,9 @@
+try:
+    num1 = int(input("Enter first number"))
+    num2 = int(input("Enter second number"))
+except:
+    print("These dont allow for letters")
+else:
+    print(f"result: {num1/num2}")
+    
+
